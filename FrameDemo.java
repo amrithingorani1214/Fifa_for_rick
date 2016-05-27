433d4d55d7180ffb56ae23bf87d7d56c1acfe465
Dan Kim <jidankim@gmail.com>
2016-05-27 10:02:33 -0400

    added menu, abandoned previous framework

--- FrameDemo.java
+++ FrameDemo.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
 import javax.swing.*;
  
 /* FrameDemo.java requires no other files. */
-public class FrameDemo {
+public class FrameDemo { 
     /**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
@@ -13,6 +13,8 @@ public class FrameDemo {
         //Create and set up the window.
         JFrame frame = new JFrame("FrameDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	JMenuBar menuBar = createMenu();
+	frame.setJMenuBar(menuBar);
  
         JLabel emptyLabel = new JLabel("");
         emptyLabel.setPreferredSize(new Dimension(175, 100));
@@ -21,6 +23,64 @@ public class FrameDemo {
         //Display the window.
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private static JMenuBar createMenu() {
+	//Where the GUI is created:
+	JMenuBar menuBar;
+	JMenu menu, submenu;
+	JMenuItem menuItem;
+
+	//Create the menu bar.
+	menuBar = new JMenuBar();
+
+	//Build the first menu.
+	menu = new JMenu("A Menu");
+	menu.setMnemonic(KeyEvent.VK_A);
+	menu.getAccessibleContext().setAccessibleDescription(
+            "The only menu in this program that has menu items");
+	menuBar.add(menu);
+
+	//a group of JMenuItems
+	menuItem = new JMenuItem("A text-only menu item",
+				 KeyEvent.VK_T);
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(
+						       KeyEvent.VK_1, ActionEvent.ALT_MASK));
+	menuItem.getAccessibleContext().setAccessibleDescription(
+								 "This doesn't really do anything");
+	menu.add(menuItem);
+
+	menuItem = new JMenuItem("Both text and icon",
+				 new ImageIcon("images/middle.gif"));
+	menuItem.setMnemonic(KeyEvent.VK_B);
+	menu.add(menuItem);
+
+	menuItem = new JMenuItem(new ImageIcon("images/middle.gif"));
+	menuItem.setMnemonic(KeyEvent.VK_D);
+	menu.add(menuItem);
+
+	//a submenu
+	menu.addSeparator();
+	submenu = new JMenu("A submenu");
+	submenu.setMnemonic(KeyEvent.VK_S);
+
+	menuItem = new JMenuItem("An item in the submenu");
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(
+						       KeyEvent.VK_2, ActionEvent.ALT_MASK));
+	submenu.add(menuItem);
+
+	menuItem = new JMenuItem("Another item");
+	submenu.add(menuItem);
+	menu.add(submenu);
+
+	//Build second menu in the menu bar.
+	menu = new JMenu("Another Menu");
+	menu.setMnemonic(KeyEvent.VK_N);
+	menu.getAccessibleContext().setAccessibleDescription(
+            "This menu does nothing");
+	menuBar.add(menu);
+	
+	return menuBar;
     }
  
     
