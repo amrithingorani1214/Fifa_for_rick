9fdabcf866dfb86f45683deb3a6c4a9bb5f1ee2b
Dan Kim <jidankim@gmail.com>
2016-06-10 10:02:48 -0400

    add team class and rewrite to text file

--- MyFrame.java
+++ MyFrame.java
@@ -1,5 +1,5 @@
-  import javax.swing.*;
-  import java.awt.*;
+import javax.swing.*;
+import java.awt.*;
 import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,54 +11,60 @@ import javax.swing.JPanel;
 import java.util.ArrayList;
 
 public class MyFrame extends JFrame {
-
+    
+    public static void main ( String[] args ) {
+	JPanel middlePanel = new JPanel ();
   
+	// create the middle panel components
+	JTextArea display = new JTextArea ( 16, 58 );
+	display.setEditable ( false ); // set textArea non-editable
+	JScrollPane scroll = new JScrollPane ( display );
+	scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+
+	//Add Textarea in to middle panel
+	middlePanel.add ( scroll );
     
-    public static void main ( String[] args )
-{
-    JPanel middlePanel = new JPanel ();
-  
+	// My code
+	JFrame frame = new JFrame("GAMEDAY");
+	frame.add ( middlePanel );
+	frame.pack ();
+	frame.setLocationRelativeTo ( null );
 
-    // create the middle panel components
-
-    JTextArea display = new JTextArea ( 16, 58 );
-    display.setEditable ( false ); // set textArea non-editable
-    JScrollPane scroll = new JScrollPane ( display );
-    scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
-
-    //Add Textarea in to middle panel
-    middlePanel.add ( scroll );
-
-        
-    // My code
-    JFrame frame = new JFrame("GAMEDAY");
-    
-    frame.add ( middlePanel );
-    frame.pack ();
-    frame.setLocationRelativeTo ( null );
-
-    
-    TextField aTextField = new TextField("");
+	TextField aTextField = new TextField("");
    
- frame.add(aTextField);
-   frame.setVisible ( true );
-       display.setLineWrap(true);
-display.setWrapStyleWord(true);
-//Team1
-ArrayList<Player> team1 = new ArrayList<Player>();
-team1.add(new Player("Abbiatti", "G", 25, 90, 90, 90));
-team1.add(new Player("Pepe", "D", 25, 90, 90, 90));
-team1.add(new Player("Ramos", "D", 25, 90, 90, 90));
-team1.add(new Player("Alaba", "D", 25, 90, 90, 90));
-team1.add(new Player("Marquinhos", "D", 25, 90, 90, 90));
-team1.add(new Player("Kaka", "M", 25, 90, 90, 90));
-team1.add(new Player("Reus", "M", 25, 90, 90, 90));
-team1.add(new Player("Ozil", "M", 25, 90, 90, 90));
-team1.add(new Player("Messi", "A", 25, 90, 90, 90));
-team1.add(new Player("Ronaldo", "A", 25, 90, 90, 90));
-team1.add(new Player("Suarez", "A", 25, 90, 90, 90));
-//Team2
-ArrayList<Player> team2 = new ArrayList<Player>();
+	frame.add(aTextField);
+	frame.setVisible ( true );
+	display.setLineWrap(true);
+	display.setWrapStyleWord(true);
+	
+	//Team1
+	ArrayList<Player> team1 = new ArrayList<Player>();
+	team1.add(new Player("Abbiatti", "G", 25, 90, 90, 90));
+	team1.add(new Player("Pepe", "D", 25, 90, 90, 90));
+	team1.add(new Player("Ramos", "D", 25, 90, 90, 90));
+	team1.add(new Player("Alaba", "D", 25, 90, 90, 90));
+	team1.add(new Player("Marquinhos", "D", 25, 90, 90, 90));
+	team1.add(new Player("Kaka", "M", 25, 90, 90, 90));
+	team1.add(new Player("Reus", "M", 25, 90, 90, 90));
+	team1.add(new Player("Ozil", "M", 25, 90, 90, 90));
+	team1.add(new Player("Messi", "A", 25, 90, 90, 90));
+	team1.add(new Player("Ronaldo", "A", 25, 90, 90, 90));
+	team1.add(new Player("Suarez", "A", 25, 90, 90, 90));
+
+	//Team2
+	ArrayList<Player> team2 = new ArrayList<Player>();
+	team2.add(TransferMarket.GoldGolie());
+	team2.add(TransferMarket.GoldDefender());
+	team2.add(TransferMarket.GoldDefender());
+	team2.add(TransferMarket.GoldDefender());
+	team2.add(TransferMarket.GoldMidfielder());
+	team2.add(TransferMarket.GoldMidfielder());
+	team2.add(TransferMarket.GoldMidfielder());
+	team2.add(TransferMarket.GoldAttacker());
+	team2.add(TransferMarket.GoldAttacker());
+	team2.add(TransferMarket.GoldAttacker());
+
+/*
 team2.add(new Player("Cech", "G", 25, 80, 80, 80));
 team2.add(new Player("Alves", "D", 25, 80, 80, 80));
 team2.add(new Player("Vermaelen", "D", 25, 80, 80, 80));
@@ -70,23 +76,25 @@ team2.add(new Player("Iniesta", "M", 25, 80, 80, 80));
 team2.add(new Player("Sanchez", "A", 25, 80, 80, 80));
 team2.add(new Player("Douglas", "A", 25, 80, 80, 80));
 team2.add(new Player("Benzema", "A", 25, 80, 80, 80));
-String text = Simulation.gamerun("Best Team",  team1 , "Bad Team", team2, team1.get(10), team2.get(10), team1.get(5), team2.get(5));   
-   String str = "";
+*/
 
-    while (text.length() > 0) {
-    str += text.substring(0,text.indexOf("\n"));
-    text = text.substring(text.indexOf("\n") + 1,text.length()-1);
-    str += "\n";
-        aTextField.setText(str);
-     try {
-    Thread.sleep(1000);                 //1000 milliseconds is one second.
-} catch(InterruptedException ex) {
-    Thread.currentThread().interrupt();
-}
+	String text = Simulation.gamerun("Best Team",  team1 , "Bad Team", team2, team1.get(10), team2.get(10), team1.get(5), team2.get(5));   
+	String str = "";
+
+	while (text.length() > 0) {
+	    str += text.substring(0,text.indexOf("\n"));
+	    text = text.substring(text.indexOf("\n") + 1,text.length()-1);
+	    str += "\n";
+	    aTextField.setText(str);
+	    try {
+		Thread.sleep(1000);                 //1000 milliseconds is one second.
+	    } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
 
 // frame.invalidate();
 //frame.validate();
-frame.repaint();
+	    frame.repaint();
 //   while (text.length() > 0) {
 //    str += text.substring(0,text.indexOf("\n"));
 
@@ -102,9 +110,10 @@ frame.repaint();
 //frame.invalidate();
 //frame.validate();
 //frame.repaint();
-}
+	}
 
-}
+    }
+
 }
   
    
